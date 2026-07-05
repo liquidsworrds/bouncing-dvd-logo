79e340a283fb1f7eeafde8fd48ea39e32088e186
liquidsworrds <120892969+liquidsworrds@users.noreply.github.com>
2026-07-05 21:30:14 +0530

    refactor collision resolution and add clamping

--- dvd.c
+++ dvd.c
@@ -65,11 +65,29 @@ void ChangeDVDColor(DVDLogo *dvdLogo) {
 }
 
 void BounceBackDVD_X(DVDLogo *dvdLogo) {
+  // Horizontal Collision Detection
+  if (dvdLogo->dstRect.x >= SCREEN_WIDTH - dvdLogo->dstRect.width) {
+    dvdLogo->dstRect.x = SCREEN_WIDTH - dvdLogo->dstRect.width;
     dvdLogo->velocity.x *= -1;
+    ChangeDVDColor(dvdLogo);
+  } else if (dvdLogo->dstRect.x <= 0) {
+    dvdLogo->dstRect.x = 0;
+    dvdLogo->velocity.x *= -1;
+    ChangeDVDColor(dvdLogo);
+  }
 }
 
 void BounceBackDVD_Y(DVDLogo *dvdLogo) {
+  // Vertical Collision Detection
+  if (dvdLogo->dstRect.y >= (SCREEN_HEIGHT - dvdLogo->dstRect.height)) {
+    dvdLogo->dstRect.y = SCREEN_HEIGHT - dvdLogo->dstRect.height;
+    ChangeDVDColor(dvdLogo);
     dvdLogo->velocity.y *= -1;
+  } else if (dvdLogo->dstRect.y <= 0) {
+    dvdLogo->dstRect.y = 0;
+    ChangeDVDColor(dvdLogo);
+    dvdLogo->velocity.y *= -1;
+  }
 }
 
 void UpdateDVDLogo(DVDLogo *dvdLogo) {
@@ -78,27 +96,8 @@ void UpdateDVDLogo(DVDLogo *dvdLogo) {
   dvdLogo->dstRect.x += dvdLogo->velocity.x * dt;
   dvdLogo->dstRect.y += dvdLogo->velocity.y * dt;
 
-  // Horizontal Collision Detection
-  if (dvdLogo->dstRect.x >= SCREEN_WIDTH - dvdLogo->dstRect.width) {
-    ChangeDVDColor(dvdLogo);
-    BounceBackDVD_X(dvdLogo);
-  }
-
-  if (dvdLogo->dstRect.x <= 0) {
-    ChangeDVDColor(dvdLogo);
-    BounceBackDVD_X(dvdLogo);
-  }
-
-  // Vertical Collision Detection
-  if (dvdLogo->dstRect.y >= (SCREEN_HEIGHT - dvdLogo->dstRect.height)) {
-    ChangeDVDColor(dvdLogo);
-    BounceBackDVD_Y(dvdLogo);
-  }
-
-  if (dvdLogo->dstRect.y <= 0) {
-    ChangeDVDColor(dvdLogo);
-    BounceBackDVD_Y(dvdLogo);
-  }
+  BounceBackDVD_X(dvdLogo);
+  BounceBackDVD_Y(dvdLogo);
 }
 
 int main(int argc, char *argv[]) {
